f5bb2207797a13c67b9e33fd70104a7b4500c786
Ciro038 <kpolak@interia.pl>
2024-02-25 15:23:31 +0100

    add comments for interfaces

--- KombajnPDF/Interfaces/IFile.cs
+++ KombajnPDF/Interfaces/IFile.cs
@@ -9,12 +9,36 @@ namespace KombajnPDF.Interfaces
 {
     internal interface IFile
     {
+        /// <summary>
+        /// File name
+        /// </summary>
         public string NameDataGridViewTextBoxColumn { get; set; }
+        /// <summary>
+        /// Path to file
+        /// </summary>
         public string PathDataGridViewTextBoxColumn { get; set; }
+        /// <summary>
+        /// Pattern
+        /// </summary>
         public string PatternDataGridViewTextBoxColumn { get; set; }
+        /// <summary>
+        /// Count of pages
+        /// </summary>
         public int TotalPagesDataGridViewTextBoxColumn { get; set; }
+        /// <summary>
+        /// Get full path to the file
+        /// </summary>
+        /// <returns>full path</returns>
         public string GetFullPath();
+        /// <summary>
+        /// Method checks pattern
+        /// </summary>
+        /// <returns>true if pattern is correct</returns>
         public bool CheckPattern();
+        /// <summary>
+        /// Method gets a list of pages to print
+        /// </summary>
+        /// <returns></returns>
         public List<int> GetPagesToPrint();
     }
 }
--- KombajnPDF/Interfaces/IFilesBindingList.cs
+++ KombajnPDF/Interfaces/IFilesBindingList.cs
@@ -12,8 +12,21 @@ namespace KombajnPDF.Interfaces
     {
         List<IFile> Items { get; }
         public IFile this[int index] { get; }
+        /// <summary>
+        /// Adds an object to the end of the collection based on full path to the file
+        /// </summary>
+        /// <param name="fullPathToFile">Full path to the file</param>
         public void Add(string fullPathToFile);
+        /// <summary>
+        /// Removes the element at the specified index od collection
+        /// </summary>
+        /// <param name="rowIndex">The zero-based index of the e,emet to remove</param>
         public void RemoveAt(int rowIndex);
+        /// <summary>
+        /// Insert an element into collection at the specified index
+        /// </summary>
+        /// <param name="index">The zero-based index at which item should be inserted</param>
+        /// <param name="fullPathToFile">Full path to the file</param>
         public void Insert(int index, string fullPathToFile);
 
     }
