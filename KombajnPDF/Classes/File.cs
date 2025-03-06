9a6113b8be032a92852462d42b179dfa2941cad7
Ciro038 <kpolak@interia.pl>
2025-03-06 12:17:26 +0100

    ref

--- KombajnPDF/Classes/File.cs
+++ KombajnPDF/Classes/File.cs
@@ -8,93 +8,91 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Net.Mime.MediaTypeNames;
 
-namespace KombajnPDF.Classes
+namespace KombajnPDF.Classes;
+/// <summary>
+/// Class representing a file
+/// </summary>
+internal class File : IFile
 {
     /// <summary>
-    /// Class representing a file
+    /// Full path to the fille
     /// </summary>
-    internal class File : IFile
+    private readonly string fullPath;
+
+    /// <summary>
+    /// File name
+    /// </summary>
+    [Browsable(true)]
+    [DisplayName("Name")]
+    public string NameDataGridViewTextBoxColumn { get; set; }
+
+    /// <summary>
+    /// Path to file
+    /// </summary>
+    [Browsable(true)]
+    [DisplayName("Path")]
+    public string PathDataGridViewTextBoxColumn { get; set; }
+
+    /// <summary>
+    /// Pattern
+    /// </summary>
+    [Browsable(true)]
+    [DisplayName("Pattern")]
+    public string PatternDataGridViewTextBoxColumn { get; set; }
+
+    /// <summary>
+    /// Count of pages
+    /// </summary>
+    [Browsable(true)]
+    [DisplayName("Total pages")]
+    public int TotalPagesDataGridViewTextBoxColumn { get; set; }
+
+    /// <summary>
+    /// Constructor creates an object based on the file path
+    /// </summary>
+    /// <param name="fullPathToFile">Full path to the file</param>
+    /// <exception cref="ArgumentNullException">If param is nothing</exception>
+    /// <exception cref="FileLoadException">if file is not a pdf</exception>
+    public File(string fullPathToFile)
     {
-        /// <summary>
-        /// Full path to the fille
-        /// </summary>
-        private readonly string fullPath;
+        if (String.IsNullOrEmpty(fullPathToFile))
+            throw new ArgumentNullException(nameof(fullPathToFile));
+        if (!System.IO.File.Exists(fullPathToFile))
+            throw new FileLoadException();
+        if (!Path.GetExtension(fullPathToFile).Equals(".PDF", StringComparison.OrdinalIgnoreCase))
+            throw new FileLoadException();
 
-        /// <summary>
-        /// File name
-        /// </summary>
-        [Browsable(true)]
-        [DisplayName("Name")]
-        public string NameDataGridViewTextBoxColumn { get; set; }
-
-        /// <summary>
-        /// Path to file
-        /// </summary>
-        [Browsable(true)]
-        [DisplayName("Path")]
-        public string PathDataGridViewTextBoxColumn { get; set; }
-
-        /// <summary>
-        /// Pattern
-        /// </summary>
-        [Browsable(true)]
-        [DisplayName("Pattern")]
-        public string PatternDataGridViewTextBoxColumn { get; set; }
-
-        /// <summary>
-        /// Count of pages
-        /// </summary>
-        [Browsable(true)]
-        [DisplayName("Total pages")]
-        public int TotalPagesDataGridViewTextBoxColumn { get; set; }
-
-        /// <summary>
-        /// Constructor creates an object based on the file path
-        /// </summary>
-        /// <param name="fullPathToFile">Full path to the file</param>
-        /// <exception cref="ArgumentNullException">If param is nothing</exception>
-        /// <exception cref="FileLoadException">if file is not a pdf</exception>
-        public File(string fullPathToFile)
-        {
-            if (String.IsNullOrEmpty(fullPathToFile))
-                throw new ArgumentNullException(nameof(fullPathToFile));
-            if (!System.IO.File.Exists(fullPathToFile))
-                throw new FileLoadException();
-            if (!Path.GetExtension(fullPathToFile).Equals(".PDF", StringComparison.CurrentCultureIgnoreCase))
-                throw new FileLoadException();
-
-            fullPath = fullPathToFile;
-            NameDataGridViewTextBoxColumn = Path.GetFileName(fullPathToFile);
-            PathDataGridViewTextBoxColumn = Path.GetDirectoryName(fullPathToFile);
-            PatternDataGridViewTextBoxColumn = "-";
-            TotalPagesDataGridViewTextBoxColumn = PdfReader.Open(fullPathToFile, PdfDocumentOpenMode.Import).PageCount;
-        }
-        /// <summary>
-        /// Method checks pattern
-        /// </summary>
-        /// <returns>true if pattern is correct</returns>
-        public bool CheckPattern()
-        {
-            var fileChecker = new FilePatternChecker();
-            return fileChecker.CheckPattern(PatternDataGridViewTextBoxColumn, TotalPagesDataGridViewTextBoxColumn);
-        }
-        /// <summary>
-        /// Method gets a list of pages to print
-        /// </summary>
-        /// <returns></returns>
-        public List<int> GetPagesToPrint()
-        {
-            var fileChecker = new FilePatternChecker();
-            fileChecker.CheckPattern(PatternDataGridViewTextBoxColumn, TotalPagesDataGridViewTextBoxColumn);
-            return fileChecker.ListOfPagesToPrint;
-        }
-        /// <summary>
-        /// Get full path to the file
-        /// </summary>
-        /// <returns>full path</returns>
-        public string GetFullPath()
-        {
-            return fullPath;
-        }
+        fullPath = fullPathToFile;
+        NameDataGridViewTextBoxColumn = Path.GetFileName(fullPathToFile);
+        PathDataGridViewTextBoxColumn = Path.GetDirectoryName(fullPathToFile);
+        PatternDataGridViewTextBoxColumn = "-";
+        TotalPagesDataGridViewTextBoxColumn = PdfReader.Open(fullPathToFile, PdfDocumentOpenMode.Import).PageCount;
+    }
+    /// <summary>
+    /// Method checks pattern
+    /// </summary>
+    /// <returns>true if pattern is correct</returns>
+    public bool CheckPattern()
+    {
+        var fileChecker = new FilePatternChecker();
+        return fileChecker.CheckPattern(PatternDataGridViewTextBoxColumn, TotalPagesDataGridViewTextBoxColumn);
+    }
+    /// <summary>
+    /// Method gets a list of pages to print
+    /// </summary>
+    /// <returns></returns>
+    public List<int> GetPagesToPrint()
+    {
+        var fileChecker = new FilePatternChecker();
+        fileChecker.CheckPattern(PatternDataGridViewTextBoxColumn, TotalPagesDataGridViewTextBoxColumn);
+        return fileChecker.ListOfPagesToPrint;
+    }
+    /// <summary>
+    /// Get full path to the file
+    /// </summary>
+    /// <returns>full path</returns>
+    public string GetFullPath()
+    {
+        return fullPath;
     }
 }
