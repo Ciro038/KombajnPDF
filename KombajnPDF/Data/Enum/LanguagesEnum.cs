d4b5a7e71ec97170c5dcb9e929082acd0e5f478a
Ciro038 <kpolak@interia.pl>
2025-07-21 17:46:34 +0200

    dodanie dodatkowych plików z tłumaczeniami

--- KombajnPDF/Data/Enum/LanguagesEnum.cs
+++ KombajnPDF/Data/Enum/LanguagesEnum.cs
@@ -20,6 +20,41 @@ namespace KombajnPDF.Data.Enum
         /// <summary>
         /// Polish language.
         /// </summary>
-        Polski = 1
+        Polski = 1,
+
+        /// <summary>
+        /// Spanish / Hiszpański / Español
+        /// </summary>
+        Spanish = 2,
+
+        /// <summary>
+        /// Chinese / Chiński / 中文");
+        /// </summary>
+        Chinese = 3,
+
+        /// <summary>
+        /// Japanese / Japoński / 日本語
+        /// </summary>
+        Japanese = 4,
+
+        /// <summary>
+        /// Ukrainian / Ukraiński / Українська
+        /// </summary>
+        Ukrainian = 5,
+
+        /// <summary>
+        /// Russian / Rosyjski / Русский
+        /// </summary>
+        Russian = 6,
+
+        /// <summary>
+        /// German / Niemiecki / Deutsch
+        /// </summary>
+        German = 7,
+
+        /// <summary>
+        /// French / Francuski / Français
+        /// </summary>
+        French = 8
     }
 }
