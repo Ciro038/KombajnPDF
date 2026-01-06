744837a9edfd405fd472231b4dfe836369cb6332
Ciro038 <kpolak@interia.pl>
2026-01-06 19:28:11 +0100

    ref

--- KombajnPDF/Data/Entity/FilesBindingList.cs
+++ KombajnPDF/Data/Entity/FilesBindingList.cs
@@ -11,35 +11,27 @@ namespace KombajnPDF.Data.Entity
 {
     internal class FilesBindingList : BindingList<IFile>, IFilesBindingList
     {
+        /// <inheritdoc/>
         public new IFile this[int index]
         {
             get { return base[index]; }
         }
+        /// <inheritdoc/>
         public new List<IFile> Items
         {
             get { return ((List<File>)base.Items).ConvertAll(file => (IFile)file); }
         }
-        /// <summary>
-        /// Adds an object to the end of the collection based on full path to the file
-        /// </summary>
-        /// <param name="fullPathToFile">Full path to the file</param>
+        /// <inheritdoc/>
         public void Add(string fullPathToFile)
         {
             Add(new File(fullPathToFile));
         }
-        /// <summary>
-        /// Removes the element at the specified index od collection
-        /// </summary>
-        /// <param name="rowIndex">The zero-based index of the e,emet to remove</param>
+        /// <inheritdoc/>
         public new void RemoveAt(int rowIndex)
         {
             base.RemoveAt(rowIndex);
         }
-        /// <summary>
-        /// Insert an element into collection at the specified index
-        /// </summary>
-        /// <param name="index">The zero-based index at which item should be inserted</param>
-        /// <param name="fullPathToFile">Full path to the file</param>
+        /// <inheritdoc/>
         public void Insert(int index, string fullPathToFile)
         {
            Insert(index, new File(fullPathToFile));
