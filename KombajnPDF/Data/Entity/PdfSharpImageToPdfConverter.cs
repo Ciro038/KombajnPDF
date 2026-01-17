ab02d74af73aae56e32eb6caf7985d75f91f59cc
Ciro038 <kpolak@interia.pl>
2026-01-17 17:07:35 +0100

    dodanie klasy PdfSharpImageToPdfConverter

---
+++ KombajnPDF/Data/Entity/PdfSharpImageToPdfConverter.cs
@@ -0,0 +1,40 @@
+﻿using KombajnPDF.App.Data.Abstract;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KombajnPDF.App.Data.Entity
+{
+    public class PdfSharpImageToPdfConverter : IImageToPdfConverter
+    {
+        public MemoryStream Convert(string imagePath)
+        {
+            if (string.IsNullOrWhiteSpace(imagePath))
+                throw new ArgumentException(nameof(imagePath));
+
+            var stream = new MemoryStream();
+
+            using var document = new PdfDocument();
+            using var image = XImage.FromFile(imagePath);
+
+            double width = image.PixelWidth * 72.0 / image.HorizontalResolution;
+            double height = image.PixelHeight * 72.0 / image.VerticalResolution;
+
+            var page = document.AddPage();
+            page.Width = width;
+            page.Height = height;
+
+            using var gfx = XGraphics.FromPdfPage(page);
+            gfx.DrawImage(image, 0, 0, width, height);
+
+            document.Save(stream, false);
+            stream.Position = 0;
+
+            return stream;
+        }
+    }
+}
