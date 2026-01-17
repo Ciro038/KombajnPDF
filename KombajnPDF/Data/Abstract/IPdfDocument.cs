90646b5b242a9f291e8e3663f5d21ff5329adb47
Ciro038 <kpolak@interia.pl>
2026-01-17 17:02:06 +0100

    dodanie interfejsu IPdfDocument

---
+++ KombajnPDF/Data/Abstract/IPdfDocument.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KombajnPDF.App.Data.Abstract
+{
+    public interface IPdfDocument : IDisposable
+    {
+        int PageCount { get; }
+        object GetPage(int index);
+    }
+}
