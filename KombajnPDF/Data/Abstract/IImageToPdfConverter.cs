b7f07cd79e8f84c70d6f4277e8f2fc98bd818a2f
Ciro038 <kpolak@interia.pl>
2026-01-17 17:01:10 +0100

    dodanie interfejsu IImageToPdfConverter

---
+++ KombajnPDF/Data/Abstract/IImageToPdfConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KombajnPDF.App.Data.Abstract
+{
+    public interface IImageToPdfConverter
+    {
+        MemoryStream Convert(string imagePath);
+    }
+}
