429dc84ea69b024a90e643801d1ab42a3fa1c833
Ciro038 <kpolak@interia.pl>
2026-01-17 17:09:21 +0100

    dodanie klasy PdfSharpDocumentWrapper  i PdfSharpLoader

---
+++ KombajnPDF/Data/Entity/PdfSharpDocumentWrapper.cs
@@ -0,0 +1,26 @@
+﻿using KombajnPDF.App.Data.Abstract;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KombajnPDF.App.Data.Entity
+{
+    public class PdfSharpDocumentWrapper : IPdfDocument
+    {
+        public PdfDocument Document { get; }
+
+        public PdfSharpDocumentWrapper(PdfDocument document)
+        {
+            Document = document;
+        }
+
+        public int PageCount => Document.PageCount;
+
+        public object GetPage(int index) => Document.Pages[index];
+
+        public void Dispose() => Document.Dispose();
+    }
+}
---
+++ KombajnPDF/Data/Entity/PdfSharpLoader.cs
@@ -0,0 +1,35 @@
+﻿using KombajnPDF.App.Data.Abstract;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KombajnPDF.App.Data.Entity
+{
+    public class PdfSharpLoader : IPdfLoader
+    {
+        public IPdfDocument CreateEmpty()
+            => new PdfSharpDocumentWrapper(new PdfDocument());
+
+        public IPdfDocument Load(string path)
+            => new PdfSharpDocumentWrapper(
+                PdfReader.Open(path, PdfDocumentOpenMode.Import));
+
+        public IPdfDocument Load(Stream stream)
+            => new PdfSharpDocumentWrapper(
+                PdfReader.Open(stream, PdfDocumentOpenMode.Import));
+
+        public void AddPage(IPdfDocument target, object page)
+        {
+            ((PdfSharpDocumentWrapper)target).Document.AddPage((PdfPage)page);
+        }
+
+        public void Save(IPdfDocument document, string path)
+        {
+            ((PdfSharpDocumentWrapper)document).Document.Save(path);
+        }
+    }
+}
