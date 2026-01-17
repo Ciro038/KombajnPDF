d7a2aee4531b74a489b0def2ea2594ddc1f8e8b4
Ciro038 <kpolak@interia.pl>
2026-01-17 17:00:32 +0100

    dodanie interfejsu IFilePatternChecker

---
+++ KombajnPDF/Data/Abstract/IFilePatternChecker.cs
@@ -0,0 +1,14 @@
+﻿using KombajnPDF.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KombajnPDF.App.Data.Abstract
+{
+    public interface IFilePatternChecker
+    {
+        bool TryParse(FileItem file, out List<int> pages);
+    }
+}
