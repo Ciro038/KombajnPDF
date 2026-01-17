f1ec0a066c2a7db7ff63b7890f61f662bb708602
Ciro038 <kpolak@interia.pl>
2026-01-17 17:02:30 +0100

    dodanie interfejsu IPdfLoader

---
+++ KombajnPDF/Data/Abstract/IPdfLoader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KombajnPDF.App.Data.Abstract
+{
+    public interface IPdfLoader
+    {
+        IPdfDocument Load(string path);
+        IPdfDocument Load(Stream stream);
+        IPdfDocument CreateEmpty();
+        void AddPage(IPdfDocument target, object page);
+        void Save(IPdfDocument document, string path);
+    }
+}
