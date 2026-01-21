b7487bd31165439f9e779c5b5c90b1c69b0bb188
Ciro038 <kpolak@interia.pl>
2026-01-21 16:30:16 +0100

    dodanie FileItemFactory klasy

---
+++ KombajnPDF.Data/Abstract/IFileItemFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KombajnPDF.Data.Abstract
+{
+    public interface IFileItemFactory
+    {
+        IFileItem Create(string path);
+    }
+}
---
+++ KombajnPDF.Data/Entity/FileItemFactory.cs
@@ -0,0 +1,14 @@
+﻿using KombajnPDF.Data.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KombajnPDF.Data.Entity
+{
+    public class FileItemFactory : IFileItemFactory
+    {
+        public IFileItem Create(string path) => new FileItem(path);
+    }
+}
